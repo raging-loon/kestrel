939c632dabf43c2a483f1baf125fc4309e3acd7b
raging-loon <85407521+raging-loon@users.noreply.github.com>
2024-07-31 14:02:18 -0400

    Updated submodule

--- libdetutil/test/unit/test_main.cpp
+++ libdetutil/test/unit/test_main.cpp
@@ -3,10 +3,9 @@
 
 TEST(StringTests, StringSearchTest)
 {
-	EXPECT_TRUE(ldu_strsrch("hello","hello",5));
+	EXPECT_TRUE(ldu_strsrch("hello", "hello", 5));
+	EXPECT_FALSE(ldu_strsrch("hello","he0ll0o",5));
 }
-
-
 
 int main(int argc, char** argv)
 {
